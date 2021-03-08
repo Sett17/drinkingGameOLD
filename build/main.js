213d73d92529e149403316d0df5c140571b02d17
Sett17 <raik.rohde@gmail.com>
2021-03-08 23:02:19 +0100

    Build and Deploy from build.py at 08.03.21 23:02

--- build/main.js
+++ build/main.js
@@ -1,4 +1,4 @@
-cacheName='build21.067.2256'
+cacheName='build.21.067.2302'
 document.querySelector('#version').innerHTML=cacheName
 const startMenu='./compos/startmenu.compo'
 const preGame='./compos/pregame.compo'
