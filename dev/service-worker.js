fbcf1ab89eecb5845aef37beafa338f31cc8ce5b
Sett17 <raik.rohde@gmail.com>
2021-03-17 00:21:35 +0100

    new service worker from workbox cli; need file cleanup and outputs in new sw! @03/17/21 00:21:02

--- dev/service-worker.js
+++ dev/service-worker.js
@@ -1,58 +1,53 @@
-cacheName = '#BUILDNUMBER#'
-const toCache = [
-	'index.html',
-	'index.html?page=startmenu',
-	'?page=startmenu',
-	'index.html?page=pregame',
-	'?page=pregame',
-	'index.html?page=play',
-	'?page=play',
-	'main.js',
-	'helpers.js',
-	'options.js',
-	'style.css',
-	'manifest.webmanifest',
-	'assets/cards.js',
-	'assets/icon-ios.png',
-	'assets/icon.webp',
-	'assets/icon.png',
-	'assets/maskable_icon.png',
-	'assets/SF-UI-Text-Regular.otf',
-	'compos/card.compo',
-	'compos/play.compo',
-	'compos/pregame.compo',
-	'compos/startmenu.compo',
-	'compos/options.compo',
-]
+var CACHE = '#BUILDNUMBER#'
 
-self.addEventListener('install', (e) => {
-	console.log('[Service Worker] Install');
-	e.waitUntil((async () => {
-		const cache = await caches.open(cacheName);
-		console.log('[Service Worker] Caching all: app shell and content');
-		await cache.addAll(toCache);
-	})());
-});
+self.addEventListener('install', function (evt) {
+	console.log('The service worker is being installed.')
+	evt.waitUntil(precache())
+})
 
-self.addEventListener('fetch', (e) => {
-	e.respondWith((async () => {
-		const r = await caches.match(e.request);
-		console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-		if (r) { return r; }
-		const response = await fetch(e.request);
-		const cache = await caches.open(cacheName);
-		console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-		cache.put(e.request, response.clone());
-		return response;
-	})());
-});
+self.addEventListener('fetch', function (evt) {
+	console.log('The service worker is serving the asset.')
+	evt.respondWith(fromCache(evt.request))
+	evt.waitUntil(update(evt.request))
+})
 
-self.addEventListener('activate', (e) => {
-	e.waitUntil((async () => {
-		const keyList = await caches.keys();
-		await Promise.all(keyList.map((key) => {
-			if (key === cacheName) { return; }
-			await caches.delete(key);
-		}))
-	})());
-});
+function precache() {
+	return caches.open(CACHE).then(function (cache) {
+		return cache.addAll([
+			'./assets/SF-UI-Text-Regular.otf',
+			'./assets/bottle.svg',
+			'./assets/cards.js',
+			'./assets/down_arrow.svg',
+			'./assets/icon-ios.png',
+			'./assets/icon.png',
+			'./assets/icon.webp',
+			'./assets/maskable_icon.png',
+			'./compos/card.compo',
+			'./compos/options.compo',
+			'./compos/play.compo',
+			'./compos/pregame.compo',
+			'./compos/startmenu.compo',
+			'./helpers.js',
+			'./index.html',
+			'./main.js',
+			'./options.js',
+			'./style.css',
+		])
+	})
+}
+
+function fromCache(request) {
+	return caches.open(CACHE).then(function (cache) {
+		return cache.match(request).then(function (matching) {
+			return matching || Promise.reject('no-match')
+		})
+	})
+}
+
+function update(request) {
+	return caches.open(CACHE).then(function (cache) {
+		return fetch(request).then(function (response) {
+			return cache.put(request, response)
+		})
+	})
+}
