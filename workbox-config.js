5a23b81e66f9e32e3455838193a43965a64359d6
Sett17 <raik.rohde@gmail.com>
2021-03-17 00:22:47 +0100

    Build and Deploy from build.py at 17.03.21 00:22

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "globDirectory": "dev/",
+  "globPatterns": [
+    "**/*.{svg,js,png,webp,otf,compo,html,webmanifest,css}"
+  ],
+  "swDest": "dev/sw.js",
+  // "ignoreURLParametersMatching": [
+  //   "/^utm_/",
+  //   "/^fbclid$/"
+  // ]
+};
